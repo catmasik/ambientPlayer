af4672b404f9bd0b2a6f1b2077ce109543c5753c
CatMasik <catmasik@gmail.com>
2019-08-25 00:11:29 +0300

    первые добавления классов  в проект

---
+++ programPlayer.cpp
@@ -0,0 +1,19 @@
+#include <QObject>
+#include "programplayer.h"
+
+class progamPlayer {
+
+
+    progamPlayer::progamPlayer (audio_task *task)
+    {
+
+    }
+
+
+    progamPlayer::~progamPlayer()
+    {
+
+    }
+
+
+};
