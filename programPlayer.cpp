0d1ffac8a2c27977084df3475af4db6073c65999
CatMasik <catmasik@gmail.com>
2019-11-20 10:56:44 +0200

    исправлен баг с ошибкой обращения к теймеру класса QMediaPlayer

--- programPlayer.cpp
+++ programPlayer.cpp
@@ -7,14 +7,12 @@
 SingleSoundPlayer::SingleSoundPlayer ( AudioTaskConfig task )
   : m_task ( task )
   , m_playlist ( new QMediaPlaylist() )
-  , m_player ( new QMediaPlayer() )
+
 {
   qDebug () << "-> " << __PRETTY_FUNCTION__;
   //progamPlayer::cur_task = task;
   m_playlist->addMedia ( QUrl::fromLocalFile ( m_task.audio_file_path ) );
   m_playlist->setPlaybackMode ( QMediaPlaylist::CurrentItemOnce );
-  m_player->setPlaylist ( m_playlist.get() );
-  m_player->setVolume ( m_task.volume );
   qDebug () << "<- " << __PRETTY_FUNCTION__;
 }
 
@@ -33,11 +31,14 @@ void SingleSoundPlayer::play()
   //qDebug () << "player first pausa"+ QString::number(progamPlayer::cur_task->first_delay) << endl;
   qDebug () << m_task.audio_file_path;
 
+  m_player.reset(new QMediaPlayer);
+  m_player->setPlaylist ( m_playlist.get() );
+  m_player->setVolume ( m_task.volume );
+
   QThread::currentThread()->sleep ( m_task.first_delay );
   m_player->play();
-  //connect ( m_player, SIGNAL (stateChanged(QMediaPlayer::State state) ), this, SLOT ( restart(QMediaPlayer::State state) ) );
 
-  connect ( *m_player.get(), SIGNAL (stateChanged(QMediaPlayer::State state) ), this, SLOT ( restart(QMediaPlayer::State state) ) );
+  connect(m_player.get(), SIGNAL(stateChanged(QMediaPlayer::State)),this, SLOT(restart(QMediaPlayer::State)));
 
   qDebug () << "<- " << __PRETTY_FUNCTION__;
 }
@@ -46,6 +47,8 @@ void SingleSoundPlayer::restart(QMediaPlayer::State state)
 {
   qDebug () << "-> " << __PRETTY_FUNCTION__;
 
+  QThread::currentThread()->sleep ( m_task.first_delay );
+  m_player->play();
 
   qDebug () << "<- " << __PRETTY_FUNCTION__;
 }
